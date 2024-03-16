24e5d03b2308a669b5f559dc527bf341b5118734
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-03-17 00:43:37 +0700

    init commit

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import React from 'react';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode></React.StrictMode>
+);
