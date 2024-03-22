8f8ebdd8cdbf1426b517234e93aad2ec60838c80
PlaksinaKate <plaksina.larisa@yandex.ru>
2024-03-22 22:36:17 +0700

    feat: add circles

--- src/pages/home/home-page.ui.tsx
+++ src/pages/home/home-page.ui.tsx
@@ -4,21 +4,63 @@ import { useScreenSize } from "../../shared/hooks/useScreenSize";
 export const HomePage = () => {
   const screenSize = useScreenSize();
   const ref = useRef<HTMLCanvasElement>(null);
+  const { width, height } = screenSize;
 
-  const draw = (context: CanvasRenderingContext2D) => {
+  const tableSize = {
+    width: width - 100,
+    height: height - 100,
+    x: 40,
+    y: 40,
+  };
+
+  const drawBilliardTable = (context: CanvasRenderingContext2D) => {
+    const { width, height, x, y } = tableSize;
     context.fillStyle = "green";
-    const { width, height } = screenSize;
-    context.fillRect(40, 40, width - 100, height - 100);
+    context.fillRect(x, y, width, height);
+  };
+
+  const drawCircles = (context: CanvasRenderingContext2D) => {
+    const { width, height, x, y } = tableSize;
+    const xEnd = x + width - 80;
+    const yEnd = y + height - 80;
+
+    for (let i = 1; i <= 16; i++) {
+      const xArc = Math.floor(Math.random() * (xEnd - x) + x + 40);
+      const yArc = Math.floor(Math.random() * (yEnd - y) + y + 40);
+      const color =
+        "rgb(" +
+        Math.round(Math.random() * 255) +
+        "," +
+        Math.round(Math.random() * 255) +
+        "," +
+        Math.round(Math.random() * 255) +
+        ")";
+      drawCircle(context, xArc, yArc, color);
+    }
+  };
+
+  const drawCircle = (
+    context: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    color: string
+  ) => {
+    context.fillStyle = color;
+    context.beginPath();
+    context.arc(x, y, 50, 0, 2 * Math.PI);
+    context.fill();
   };
 
   useEffect(() => {
-    const { width, height } = screenSize;
     const canvas = ref.current;
     if (canvas) {
       canvas.width = width - 50;
       canvas.height = height - 50;
-      const context = canvas?.getContext("2d");
-      if (context) draw(context);
+      const context = canvas.getContext("2d");
+      if (context) {
+        drawBilliardTable(context);
+        drawCircles(context);
+      }
     }
   });
 
